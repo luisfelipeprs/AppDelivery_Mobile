d5c678a16e98350578ebd1e2614b40d476fe0ecc
luisfelipeprs <feliperaxz@gmail.com>
2024-10-07 19:32:37 -0300

    feat(driver): funçao de login e autenticaçao da entidade driver

---
+++ services/LoginDriver.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+export default async function LoginCompany(email: string, password: string) {
+  try {
+    const dominio = await AsyncStorage.getItem("domain")
+    
+    if (!dominio) {
+      throw new Error('Domínio ou porta não encontrados no AsyncStorage');
+    }
+
+    const url = `${dominio}/driver/login`;
+
+    const data = {
+      email,
+      password,
+    };
+      
+    const response = await axios.post(url, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    return response.data;
+  } catch (error) {
+    // Trata erros da requisição
+    console.error('Erro no login do motorista:', error);
+    throw error;
+  }
+}
